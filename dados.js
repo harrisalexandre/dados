73ab6af3b40213404ec364b18851de78c07d28ff
Harris Alexandre <50559815+harrisalexandre@users.noreply.github.com>
2023-06-10 23:12:41 -0300

    add feature terceira rolagem

--- dados.js
+++ dados.js
@@ -1,6 +1,7 @@
 var dados;
 var dado1;
 var dado2;
+var dado3;
 
 function carregarDados() {
   // Faz a requisição AJAX para buscar os dados do JSON
@@ -11,9 +12,25 @@ function carregarDados() {
       dados = JSON.parse(request.responseText);
       dado1 = dados.dado1;
       dado2 = dados.dado2;
+      dado3 = dados.dado3; // adiciona esta linha para carregar o terceiro dado
     }
   }
   request.send();
+}
+
+function terceiraRolagem() {
+  // Gera uma terceira rolagem randômica
+  const num3 = Math.floor(Math.random() * dado3.length);
+  const rolagem3 = dado3[num3];
+
+  // Exibe o resultado ao usuário
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `
+     <p>
+     <mark style="background-color: #c48ded;">${rolagem3.acao}
+     </mark>.
+     </p>
+   `;
 }
 
 function sortear() {
@@ -21,20 +38,26 @@ function sortear() {
   const num1 = Math.floor(Math.random() * dado1.length);
   const rolagem1 = dado1[num1];
 
-  // Gera um local randômico
-  const num2 = Math.floor(Math.random() * dado2.length);
-  const rolagem2 = dado2[num2];
+  // Verifica se o valor da primeira rolagem é igual a um valor específico
+  if (rolagem1.acao === "3") {
+    // Chama uma terceira função de rolagem
+    terceiraRolagem();
+  } else {
+    // Gera um local randômico
+    const num2 = Math.floor(Math.random() * dado2.length);
+    const rolagem2 = dado2[num2];
 
-  // Exibe o resultado ao usuário
-  const resultado = document.getElementById("resultado");
-  resultado.innerHTML = `
-     <p>
-     <mark style="border-radius: 5px; background-color: #c48ded;">${rolagem1.acao}
-     </mark> 
-     <mark style="border-radius: 5px; background-color: #c48ded;">${rolagem2.onde}
-     </mark>.
-     </p>
-   `;
+    // Exibe o resultado ao usuário
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `
+       <p>
+       <mark style=" background-color: #c48ded;">${rolagem1.acao}
+       </mark> 
+       <mark style=" background-color: #c48ded;">${rolagem2.onde}
+       </mark>.
+       </p>
+     `;
+  }
 }
 
 carregarDados();
